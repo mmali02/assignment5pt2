4444d1ad1da48448265bdb99baf3df27ab80d68e
mmali02 <mmali@westmont.edu>
2023-12-14 21:50:07 -0800

    made changes, it is not fully functional

--- src/main_work/organizing.py
+++ src/main_work/organizing.py
@@ -86,14 +86,19 @@ class FeatureSet:
         # Original features based on word occurrences
         original_features = {Feature(word): True for word in words}
 
-        # Additional features for pre-classified positive and negative words
-        # Customize these lists based on Twitter sentiment characteristics
-        positive_words = {'awesome', 'love', 'amazing', 'happy', 'great', 'fun', 'exciting', 'cool', 'fantastic'}
-        negative_words = {'sad', 'hate', 'terrible', 'bad', 'horrible', 'not', 'why', 'meh', 'unpleasant', 'worst'}
+        # Additional features for pre-classified positive, neutral and negative words
+        positive_words = {'awesome', 'love', 'amazing', 'happy', 'great', 'fun', 'exciting', 'cool', 'fantastic', 'excellent' }
+
+        neutral_words = {'meh', 'okay', 'alright', 'acceptable', 'uninterested', 'uh', 'even', 'different', 'something' }
+
+        negative_words = {'sad', 'hate', 'terrible', 'bad', 'horrible', 'not', 'why', 'unpleasant', 'worst', 'poor', 'shit'}
 
         # Add features based on the presence of positive and negative words in the text
         for word_feature in positive_words:
             original_features[Feature(f"positive_{word_feature}")] = word_feature in words
+
+        for word_feature in neutral_words:
+            original_features[Feature(f"neutral_{word_feature}")] = word_feature in words
 
         for word_feature in negative_words:
             original_features[Feature(f"negative_{word_feature}")] = word_feature in words
@@ -114,26 +119,23 @@ class NaiveBayesTextClassifier(AbstractClassifier):
 
     def gamma(self, a_feature_set: FeatureSet) -> str:
         """
-        Calculate the posterior probability for each class based on the given feature set.
+            Calculate the posterior probability for each class based on the given feature set.
 
-        :param a_feature_set: An instance of FeatureSet containing relevant features for classification.
-        :return: The predicted class based on the highest posterior probability.
-        """
+            :param a_feature_set: An instance of FeatureSet containing relevant features for classification.
+            :return: The predicted class based on the highest posterior probability.
+            """
         # Initialize a dictionary to store posterior probabilities for each class
         posterior_probs = {}
-
         # Calculate posterior probabilities for each class
         for clas, prior in self.class_priors.items():
             # Initialize likelihood with a default value of 0.5
             likelihood = 0.5
-
             # Iterate through each feature in the feature set
             for feature in a_feature_set.feat:
                 # Check if the feature has likelihood information for the current class
                 if feature.name in self.feature_likelihoods and clas in self.feature_likelihoods[feature.name]:
                     # Update likelihood based on the feature's likelihood for the current class
                     likelihood *= self.feature_likelihoods[feature.name][clas]
-
             # Calculate the overall posterior probability for the current class
             posterior_probs[clas] = prior * likelihood
 
@@ -144,11 +146,11 @@ class NaiveBayesTextClassifier(AbstractClassifier):
 
     def present_features(self, top_n: int = 1) -> List[Dict[str, Any]]:
         """
-        Present the top N informative features and their probabilities based on the trained classifier.
+            Present the top N informative features and their probabilities based on the trained classifier.
 
-        :param top_n: The number of top informative features to display. Default is 1.
-        :return: A list of dictionaries containing feature information, class, and probability.
-        """
+            :param top_n: The number of top informative features to display. Default is 1.
+            :return: A list of dictionaries containing feature information, class, and probability.
+            """
         # Check if the classifier has been trained
         if not self.feature_likelihoods:
             print("Classifier has not been trained yet. Call the train method first.")
@@ -175,20 +177,18 @@ class NaiveBayesTextClassifier(AbstractClassifier):
 
     def train(self, training_set: Iterable[FeatureSet]) -> 'NaiveBayesTextClassifier':
         """
-        Train the Naive Bayes text classifier using the provided training set.
+           Train the Naive Bayes text classifier using the provided training set.
 
-        :param training_set: An iterable containing instances of FeatureSet for training the classifier.
-        :return: The trained NaiveBayesTextClassifier instance.
-        """
+           :param training_set: An iterable containing instances of FeatureSet for training the classifier.
+           :return: The trained NaiveBayesTextClassifier instance.
+           """
         # Calculate class priors
         total_samples = 0
         class_counts = {}
-
         # Count occurrences of each class in the training set
         for data_point in training_set:
             total_samples += 1
             class_counts[data_point.clas] = class_counts.get(data_point.clas, 0) + 1
-
         # Calculate and store class priors
         for clas, count in class_counts.items():
             self.class_priors[clas] = count / total_samples
@@ -196,41 +196,27 @@ class NaiveBayesTextClassifier(AbstractClassifier):
         # Calculate feature likelihoods
         feature_counts = {}
         class_totals = {}
-
         # Iterate through the training set to count features for each class
         for data_point in training_set:
             class_totals[data_point.clas] = class_totals.get(data_point.clas, 0) + 1
-
             for feature in data_point.feat:
                 feature_name = feature.name
 
-                # Include category feature
-                if feature_name == 'category':
-                    feature_value = feature.value
-                    if feature_value not in feature_counts:
-                        feature_counts[feature_value] = {}
-                    if data_point.clas not in feature_counts[feature_value]:
-                        feature_counts[feature_value][data_point.clas] = 0
-                    feature_counts[feature_value][data_point.clas] += 1
+                # Handle pre-classified positive and negative word features separately
+                if feature_name.startswith("positive_") or feature_name.startswith("neutral_") or feature_name.startswith("negative_"):
+                    if feature_name not in feature_counts:
+                        feature_counts[feature_name] = {}
+                    if data_point.clas not in feature_counts[feature_name]:
+                        feature_counts[feature_name][data_point.clas] = 0
+                    feature_counts[feature_name][data_point.clas] += feature.value
 
                 # Handle other features
                 else:
-                    # Handle pre-classified positive and negative word features separately
-                    if feature_name.startswith("positive_") or feature_name.startswith("negative_"):
-                        if feature_name not in feature_counts:
-                            feature_counts[feature_name] = {}
-                        if data_point.clas not in feature_counts[feature_name]:
-                            feature_counts[feature_name][data_point.clas] = 0
-                        feature_counts[feature_name][data_point.clas] += feature.value
-
-                    # Handle other features
-                    else:
-                        if feature_name not in feature_counts:
-                            feature_counts[feature_name] = {}
-                        if data_point.clas not in feature_counts[feature_name]:
-                            feature_counts[feature_name][data_point.clas] = 0
-                        feature_counts[feature_name][data_point.clas] += 1
-
+                    if feature_name not in feature_counts:
+                        feature_counts[feature_name] = {}
+                    if data_point.clas not in feature_counts[feature_name]:
+                        feature_counts[feature_name][data_point.clas] = 0
+                    feature_counts[feature_name][data_point.clas] += 1
         # Calculate and store feature likelihoods using Laplace smoothing
         for feature_name, class_counts in feature_counts.items():
             if feature_name not in self.feature_likelihoods:
@@ -238,6 +224,5 @@ class NaiveBayesTextClassifier(AbstractClassifier):
             for clas, count in class_counts.items():
                 # Use Laplace smoothing
                 self.feature_likelihoods[feature_name][clas] = (count + 1) / (class_totals[clas] + len(feature_counts))
-
         # Return the trained classifier instance
         return self
--- src/main_work/running.py
+++ src/main_work/running.py
@@ -9,52 +9,54 @@ import os
 import random
 
 
+
 nltk.download('twitter_samples')
 
 
 class TwitterRunner:
     def __init__(self):
         self.classifier = NaiveBayesTextClassifier()
-        self.data = self.load_twitter_data()
+        self.data = self.load_tweet_data()
         self.output_directory = "out"  # Output directory
 
-    def load_twitter_data(self, test_size=0.2) -> tuple[List[FeatureSet], List[FeatureSet]]:
+    def load_tweet_data(self, test_size=0.2) -> tuple[List[FeatureSet], List[FeatureSet]]:
         """
-        Load twitter_samples data and split it into training and testing sets.
+                Load movie reviews data and split it into training and testing sets.
 
-        Args:
-            test_size: Proportion of the dataset to include in the test split.
+                Args:
+                    test_size: Proportion of the dataset to include in the test split.
 
-        Returns:
-            Tuple of training and testing data.
-        """
+                Returns:
+                    Tuple of training and testing data.
+                """
 
-        positive_tweets = [(tweet, 'pos') for tweet in twitter_samples.strings('positive_tweets.json')]
-        negative_tweets = [(tweet, 'neg') for tweet in twitter_samples.strings('negative_tweets.json')]
-        all_tweets = positive_tweets + negative_tweets
-        min_samples = min(len(positive_tweets), len(negative_tweets))
-        random.shuffle(all_tweets)
+        positive_fileids = twitter_samples.fileids('pos')
+        neutral_fileids = twitter_samples.fileids('neu')
+        negative_fileids = twitter_samples.fileids('neg')
+        all_fileids = positive_fileids + negative_fileids + neutral_fileids
+        min_samples = min(len(positive_fileids), len(negative_fileids), len(neutral_fileids))
+        random.shuffle(all_fileids)
 
-        tweets = [
-            FeatureSet.build(tweet, known_clas=label) for tweet, label in all_tweets[:min_samples]
-        ]
+        reviews = [
+            FeatureSet.build(twitter_samples.raw(fileid), known_clas='pos' if fileid in positive_fileids else 'neg') for
+            fileid in all_fileids[:min_samples]]
 
         # Split the data into training and testing sets
-        train_data, test_data = train_test_split(tweets, test_size=test_size, random_state=42)
+        train_data, test_data = train_test_split(reviews, test_size=test_size, random_state=42)
 
         return train_data, test_data
 
     def train_classifier(self):
         self.classifier.train(self.data[0])  # Training data
 
-    def classify_tweets(self) -> List[tuple]:
+    def classify_reviews(self) -> List[tuple]:
         results = []
         correct_predictions = 0
 
-        for tweet in self.data[1]:  # Testing data
-            predicted_label = self.classifier.gamma(tweet)
-            results.append((tweet.clas, predicted_label))
-            if predicted_label == tweet.clas:
+        for review in self.data[1]:  # Testing data
+            predicted_label = self.classifier.gamma(review)
+            results.append((review.clas, predicted_label))
+            if predicted_label == review.clas:
                 correct_predictions += 1
 
         # Calculate accuracy
@@ -65,26 +67,32 @@ class TwitterRunner:
         return [feature['feature'].name for feature in self.classifier.present_features(top_n)]
 
     def print_results_to_file(self, results: List[tuple], accuracy: float, top_features: List[str],
-                              output_file: str = "twitter_runner_results.txt"):
+                              output_file: str = "tweet_runner_results.txt"):
         output_path = os.path.join(self.output_directory, output_file)
 
         # Calculate counts for true positives, true negatives, false positives, and false negatives
         true_positives = sum((actual == 'pos' and predicted == 'pos') for actual, predicted in results)
+        true_neutrals = sum((actual == 'neu' and predicted == 'neu') for actual, predicted in results)
         true_negatives = sum((actual == 'neg' and predicted == 'neg') for actual, predicted in results)
         false_positives = sum((actual == 'neg' and predicted == 'pos') for actual, predicted in results)
+        false_positives2 = sum((actual == 'neu' and predicted == 'pos') for actual, predicted in results)
         false_negatives = sum((actual == 'pos' and predicted == 'neg') for actual, predicted in results)
+        false_negatives2 = sum((actual == 'neu' and predicted == 'neg') for actual, predicted in results)
 
         with open(output_path, 'w') as file:
-            file.write("TwitterRunner Results:\n")
+            file.write("Twitter_Runner Results:\n")
             file.write(f"Accuracy: {accuracy * 100:.2f}%\n")
             file.write("\nTop Features:\n")
             for feature in top_features:
                 file.write(f"{feature}\n")
             file.write("\nCounts:\n")
             file.write(f"True Positives: {true_positives}\n")
+            file.write(f"True Neutrals: {true_neutrals}\n")
             file.write(f"True Negatives: {true_negatives}\n")
             file.write(f"False Positives: {false_positives}\n")
+            file.write(f"False Positives 2: {false_positives2}\n")
             file.write(f"False Negatives: {false_negatives}\n")
+            file.write(f"False Negatives 2: {false_negatives2}\n")
 
     def run(self):
         # Create the output directory if it doesn't exist
@@ -92,18 +100,20 @@ class TwitterRunner:
             os.makedirs(self.output_directory)
 
         self.train_classifier()
-        results, accuracy = self.classify_tweets()
+        results, accuracy = self.classify_reviews()
 
         # Extract length feature for printing
         length_feature = next(iter(self.data[1][0].feat))  # Assuming the first feature is the length
         top_features = self.determine_top_features()
-        top_features.append(f"Tweet Length: {length_feature.value}")
+        top_features.append(f"Review Length: {length_feature.value}")
 
         # Extract pre-classified positive and negative word features for printing
         positive_word_features = [feature for feature in top_features if feature.startswith("positive_")]
+        neutral_word_features = [feature for feature in top_features if feature.startswith("neutral_")]
         negative_word_features = [feature for feature in top_features if feature.startswith("negative_")]
 
         print(f"\nPre-classified Positive Words: {positive_word_features}")
+        print(f"Pre-classified Neutral Words: {neutral_word_features}")
         print(f"Pre-classified Negative Words: {negative_word_features}")
 
         self.print_results_to_file(results, accuracy, top_features)
@@ -115,11 +125,11 @@ class TwitterRunner:
         print("\nTop Features:")
         for feature in top_features:
             print(feature)
-        print("\nTweet Details:")
+        print("\nReview Details:")
         for actual, predicted in results:
             print(f"Actual: {actual}, Predicted: {predicted}")
 
 
 if __name__ == "__main__":
-    twitter_runner = TwitterRunner()
-    twitter_runner.run()
+    tweet_runner = TwitterRunner()
+    tweet_runner.run()
